55669e275d0714b8e56b8abfe86ca098ac797624
igorbiliba <igorbiliba@mail.ru>
2018-11-16 17:48:20 +0300

    +

--- keepalive_cron_every_min.php
+++ keepalive_cron_every_min.php
@@ -30,7 +30,7 @@ class Demon {
 
 class Process {    
     public function start($processName) {        
-        exec("cd ".__DIR__." && nohup $processName manage.py runserver --host=0.0.0.0 & 2> /dev/null");
+        exec("su wmbroker \"nohup $processName ".__DIR__."/manage.py runserver --host=0.0.0.0 &\"");
     }
     
     public function getPids($processName) {
